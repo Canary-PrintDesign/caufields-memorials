6836c5bbed743d74d547db08fab1267e6f60d86d
Kirsten Dodd <kirsten.jvd@gmail.com>
2017-08-13 15:25:18 -0700

    Updates to estimates js flow, colors added, active state supplied

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -4,7 +4,7 @@ console.log('Welcome to Caufield Memorials!');
 
 $(document).ready(function(){
 
-	var height = $( window ).height() - 221;
+	var height = $( window ).height() - 151;
 	$(".estimate section").css("height",height);
 	
 	$(".step-2 .product-container").not(".default").hide();
@@ -24,33 +24,35 @@ $(document).ready(function(){
   	$(product).show();
 		$(product).siblings("div").hide();
 
-    $('html, body').animate({
-      scrollTop: $(product).offset().top - headerHeight
-    }, 1000, function() {
-      // Callback after  animation
-      // Must change focus!
-      // var $product = $(product);
-      // $product.focus();
-      // if ($product.is(":focus")) { // Checking if the target was focused
-      //   return false;
-      // } else {
-      //   $product.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-      //   $product.focus(); // Set focus again
-      // };
-    });
+    // $('html, body').animate({
+    //   scrollTop: $(product).offset().top - headerHeight
+    // }, 500, function() {
+    //   // Callback after  animation
+    //   // Must change focus!
+    //   // var $product = $(product);
+    //   // $product.focus();
+    //   // if ($product.is(":focus")) { // Checking if the target was focused
+    //   //   return false;
+    //   // } else {
+    //   //   $product.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+    //   //   $product.focus(); // Set focus again
+    //   // };
+    // });
 
   });
 
-  $(".step-2 .selection-container").click(function(){
+  $(".selection-container").click(function(){
 
     var fuller = $(this).closest('section').next();
     var headerHeight = 161;
 
      $('html, body').animate({
       scrollTop: $(fuller).offset().top - headerHeight
-    }, 1000);
+    }, 500);
 
-   });
+
+
+  });
 
 });
 
