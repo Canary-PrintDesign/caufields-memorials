7caa1cb125143e13a4cf8b759971eef0994a0ba2
Kirsten Dodd <kirsten.jvd@gmail.com>
2017-08-13 01:10:45 -0700

    estimate page script setup

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -1,1 +1,31 @@
-console.log('Welcome to Highridge Homes!');
+require('jquery');
+
+console.log('Welcome to Caufield Memorials!');
+
+$(document).ready(function(){
+	
+  $(".selection-container").click(function(){
+    $(this).toggleClass('active');
+    $(this).siblings().removeClass('active');
+
+    var product = $(this).children('a').attr("href");
+		$(product).show();
+
+    $('html, body').animate({
+      scrollTop: $(product).offset().top
+    }, 1000, function() {
+      // Callback after  animation
+      // Must change focus!
+      var $product = $(product);
+      $product.focus();
+      if ($product.is(":focus")) { // Checking if the target was focused
+        return false;
+      } else {
+        $product.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+        $product.focus(); // Set focus again
+      };
+    });
+  });
+
+});
+
