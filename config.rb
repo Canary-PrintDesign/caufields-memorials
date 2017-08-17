ac9b7a2f2f909e83763d5d89ab1684cdb828b14e
adamish717 <adam@piechnik.ca>
2017-08-17 11:28:16 -0700

    Add S3 sync

--- config.rb
+++ config.rb
@@ -28,3 +28,18 @@ activate :asset_hash
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
 set :images_dir, 'images'
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'www.caufieldsmemorials.com'
+  s3_sync.region                     = 'us-west-2'
+  s3_sync.aws_access_key_id          = 'REDACTED: Use .s3_sync file with info in it'
+  s3_sync.aws_secret_access_key      = 'REDACTED: Use .s3_sync file with info in it'
+  s3_sync.delete                     = true
+  s3_sync.prefer_gzip                = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.prefix                     = ''
+  s3_sync.version_bucket             = false
+end
+
+require 'mime-types'
