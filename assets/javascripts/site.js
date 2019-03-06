b2258c3ab264fb2358b22b1cc86d8fb7f861b2b0
Adam J. Piechnik <adam@piechnik.ca>
2019-03-06 11:50:36 -0800

    cleanup JS file

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -5,7 +5,7 @@ console.log('Welcome to Caufield Memorials!');
 var headerHeight = $(".nav-header").height() + 40;
 var height = $( window ).height() - headerHeight;
 
-$(window).scroll(function() {    
+$(window).scroll(function() {
   var scroll = $(window).scrollTop();
 
   if (scroll >= height) {
@@ -13,7 +13,7 @@ $(window).scroll(function() {
   } else {
   	$(".index .nav-header").addClass("partial-fixed");
   }
-}); 
+});
 
 $(document).ready(function(){
 	if ($(window).width() > 900) {
@@ -21,13 +21,13 @@ $(document).ready(function(){
 	}else {
 		$("body").css("padding-top",headerHeight);
 	}
-	
-	$(".step-2 .product-container").not(".default").hide();	
+
+	$(".step-2 .product-container").not(".default").hide();
   $(".selection-container").click(function(){
     $(this).toggleClass('active');
     $(this).siblings().removeClass('active');
   });
-  
+
   $(".bronze-plaque").click(function(){
     $('#free-estimate').addClass('bronze-plaque');
   });
@@ -38,43 +38,22 @@ $(document).ready(function(){
   });
 
   $(".step-1 .selection-container").click(function(){
- 	
   	var product = $(this).children('div').attr("class");
     var productId = '#'+product;
   	var headerHeight = 221;
 
   	$(productId).show();
 		$(productId).siblings("div").hide();
-
-    // $('html, body').animate({
-    //   scrollTop: $(product).offset().top - headerHeight
-    // }, 500, function() {
-    //   // Callback after  animation
-    //   // Must change focus!
-    //   // var $product = $(product);
-    //   // $product.focus();
-    //   // if ($product.is(":focus")) { // Checking if the target was focused
-    //   //   return false;
-    //   // } else {
-    //   //   $product.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-    //   //   $product.focus(); // Set focus again
-    //   // };
-    // });
-
   });
 
-  
 
   $(".selection-container").click(function(){
-
     var fuller = $(this).closest('section').next();
     var headerHeight = 161;
 
      $('html, body').animate({
       scrollTop: $(fuller).offset().top - headerHeight
     }, 500);
-
-
   });
 
   $('.process-item p').each(function(event){
@@ -89,12 +68,12 @@ $(document).ready(function(){
                 '<a href="#" class="read_more">...<br>Read More</a>'+
                 '<span class="more_text" style="display:none;">'+long_content+'</span>');
 
-        $(this).find('a.read_more').click(function(event){ 
+        $(this).find('a.read_more').click(function(event){
             event.preventDefault();
             $(this).hide();
             $(this).parents('.process-item').find('.more_text').show();
-        }); 
-    }   
+        });
+    }
   });
 
   // Set up an event listener for the contact form.
@@ -127,56 +106,10 @@ $(document).ready(function(){
         $(formMessages).removeClass('success');
         $('form button').text('Please Try Again');
         $(formMessages).text('Oops! An error occured and your message could not be sent.');
-      
+
       }
     })
 
   });
 
-});  
-
-
-
-// $('form').on("submit", function (e) {
-  
-//   e.preventDefault();
-
-//   // Initiate Variables With Form Content
-//   var formData = $('form').serialize();
-
-//   $.ajax({
-//     type: "POST",
-//     url: $('form').attr('action'),
-//     data: formData
-//     success : function(text){
-//       if (text == "success"){
-//           formSuccess();
-//       } else {
-//         formError();
-//         submitMSG(false,text);
-//       }
-//     }
-//   });
-
-//   function formSuccess(){
-//     $('form')[0].reset();
-//     submitMSG(true, "Message Submitted!")
-//   }
-
-//   function formError(){
-//     $('form').removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-//         $(this).removeClass();
-//     });
-//   }
-
-//   function submitMSG(valid, msg){
-//     if(valid){
-//         var msgClasses = "h3 text-center tada animated text-success";
-//     } else {
-//         var msgClasses = "h3 text-center text-danger";
-//     }
-//     $("#form-messages").removeClass().addClass(msgClasses).text(msg);
-//   }
-
-// });
-
+});
